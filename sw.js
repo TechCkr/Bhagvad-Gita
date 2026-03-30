382603a9f0fff3d5c47f82c514456d74a887a52f
TechCkr <shaktidas.das@gmail.com>
2026-03-30 09:29:30 +0530

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener("install", () => self.skipWaiting());
-self.addEventListener("activate", e => e.waitUntil(self.clients.claim()));
-self.addEventListener("fetch", e => {
-  if (e.request.mode === "navigate") {
-    e.respondWith(fetch(e.request).catch(()=>caches.match("./index.html")));
-    return;
-  }
-  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
-});
